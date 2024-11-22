ceeae9550c1a4870dab1e7c78f25807c0e80720f
nitish kumar <nitishkumar7645828185.com>
2024-11-22 19:08:04 +0530

    first

--- src/data/usersData.js
+++
@@ -1,5 +0,0 @@
-export const users = [
-  { id: 1, username: "admin", role: "Admin", status: "Active" },
-  { id: 2, username: "john_doe", role: "Editor", status: "Inactive" },
-  // Add more users as needed
-];
